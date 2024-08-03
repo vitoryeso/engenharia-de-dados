b110e3ba4f21666e36bf71506b16f93a4a7d0563
vitoryeso <vitoryeso@outlook.com>
2024-08-03 13:13:35 -0300

    update download datasets

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,1 +1,6 @@
-wget <urls>
+mkdir datasets
+cd datasets
+
+wget https://www.dca.ufrn.br/~viegas/disciplinas/DCA0132/files/Datasets/salarios.csv
+wget https://www.dca.ufrn.br/~viegas/disciplinas/DCA0132/files/Datasets/dovecot.log
+
