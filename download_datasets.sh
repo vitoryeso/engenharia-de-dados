b699ad5bbb6b18031c8209aca39bfdf052e61d95
vyeso <vitoryeso@gmail.com>
2024-08-08 19:08:41 -0300

    lab 8 spark e mongo

--- download_datasets.sh
+++ download_datasets.sh
@@ -4,4 +4,5 @@ cd datasets
 wget https://www.dca.ufrn.br/~viegas/disciplinas/DCA0132/files/Datasets/salarios.csv
 wget https://www.dca.ufrn.br/~viegas/disciplinas/DCA0132/files/Datasets/dovecot.log
 wget https://www.dca.ufrn.br/~viegas/disciplinas/DCA0132/files/Datasets/conjunto2.csv
+wget https://dados.ufrn.br/dataset/554c2d41-cfce-4278-93c6-eb9aa49c5d16/resource/d271d842-929e-4d85-a1a8-c89cbdadb95d/download/discentes-2024.csv
 
