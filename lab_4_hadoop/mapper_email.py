ca195a7c5edb906f1029bcb50c36cfeb947bae80
vitoryeso <vitoryeso@outlook.com>
2024-08-08 10:56:07 -0300

    update mapper

--- lab_4_hadoop/mapper_email.py
+++ lab_4_hadoop/mapper_email.py
@@ -4,12 +4,9 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     
-    words = line.split("dovecot: ")[1:]
-    if len(words) >= 2:  # Certifique-se de que há pelo menos dois campos
-        nome = words[0] + ", " + words[1]
-        try:
-            salario = float(words[-2].replace('$', '').strip())  # Remove símbolo de moeda e converte para float
-        except ValueError as err:
-            print(f"ValueError: {err}", file=sys.stderr)
-            continue  # Ignore linhas com valores inválidos de salário
-        print(f"{salario}\t{nome}")
+    if "dovecot: " in line:
+        if "Jun " in line:
+            if "imap-login" in line:
+                user_str = line.split(" ")[7]
+                username = user_str.split("=<")[-1].replace(">,", "")
+                print(f"1\t{username}")
